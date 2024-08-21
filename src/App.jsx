866d0e88f2673972d052a2a3867bce412d18ab6f
Joel <ajiolajoel@gmail.com>
2024-08-21 22:11:14 +0100

    installed react, react-dom, and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Register from './components/Register';
+import SuperAdmin from './components/SuperAdmin';
+import Supervisor from './components/Supervisor';
+
+
+function App() {
+  return (
+    <>
+    <div className='text-blue-700'>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element ={<Home/>} />
+      <Route path ='/register' element={<Register/>} />
+      <Route path='/superadmin' element={<SuperAdmin/>} />
+      <Route path='/supervisor' element={<Supervisor/>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: ['./index.html',
+   "./src/*.{jsx,js}"],
+  theme: {
+    screens: {
+      '2xl': { 'max': '1535px' },
+      'xl': { 'max': '1279px' },
+      'lg': { 'max': '1023px' },
+      'md': { 'max': '767px' },
+      'sm': { 'max': '428px' },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
