59a1f3217a3a098c89718e75b8dec1ce133981bf
samuel_x <taiwosamuel504@gmail.com>
2024-09-06 21:10:07 +0100

    route now protected

--- Backend/app.js
+++ Backend/app.js
@@ -2,28 +2,23 @@ require("dotenv").config();
 require("express-async-errors");
 const cors = require("cors");
 const helmet = require("helmet");
+
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
 const student = require('./models/student.js');
-const router = require('./routes/index.js');
+
 
 const app = express();
-
-//middlewares
 app.use(express.json());
-app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-const app = express();
-app.use(express.json());
 //database
 const connectDB = require("./db/connect");
 
 //routes
 const authRouter = require("./routes/authRoute");
+const matricRoute = require("./routes/matricRoute.js");
 
 //middleware
 const notFound = require("./middleware/not-found");
@@ -35,23 +30,25 @@ app.use(morgan("tiny"));
 app.use(helmet());
 
 app.use(cors());
-app.use(cookieParser(process.env.JWT_SECRET)); // signing our cookies
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// signing our cookies
 
 app.get("/", (req, res) => {
   console.log(req.signedCookies);
   res.send("FaceEdu Api");
 });
 
-app.use("/api/v1/auth", authRouter);
-
+app.use(authRouter);
+app.use(matricRoute);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
->>>>>>> dffd0868dbf56f1b0530feb6b3d108ccbdb32aa0
+
 
 const port = process.env.PORT || 5000;
 
-// routes
-app.use('/', router);
 
 const start = async () => {
   try {
